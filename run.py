950744887ad16e5e640175197df0406f5b8f24ca
JoseLM0 <152379622+JoseLM0@users.noreply.github.com>
2023-11-30 09:11:41 +0100

    NUEVAS RUTAS

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
-from flask import Flask       
+from flask import Flask, render_template           
 
 app = Flask(__name__)    
 
 @app.route('/', methods={'GET'})    
 def holamundo():
-    return 'Hola Mundocl' 
+    return render_template('/index.html') 
 
 @app.route('/mis_proyectos', methods=['GET'])    
 def mostrarproyectos():
