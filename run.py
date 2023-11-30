09af0ea8555a504f763c26d0cebb206d768083e5
JoseLM0 <152379622+JoseLM0@users.noreply.github.com>
2023-11-30 12:37:38 +0100

    0.01

--- run.py
+++ run.py
@@ -3,12 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)    
 
 @app.route('/', methods={'GET'})    
-def holamundo():
+def index():
     return render_template('/index.html') 
 
-@app.route('/mis_proyectos', methods=['GET'])    
-def mostrarproyectos():
-    return 'Aqui se muestran los proyectos'
+@app.route('/agenda', methods=['GET'])    
+def agenda():
+    return render_template('agenda.html')
+
+@app.route('/contacto', methods=['GET'])    
+def contacto():
+    return render_template('contacto.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host = '127.0.0.1', port = '5001')  
