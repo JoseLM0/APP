a00addfd1318166d4aa49e3b01a08915f988aaac
JoseLM0 <152379622+JoseLM0@users.noreply.github.com>
2023-11-30 13:29:24 +0100

    0.03

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template           
+from flask import Flask, render_template, request              
 
 app = Flask(__name__)    
 
-@app.route('/', methods={'GET'})    
+@app.route('/', methods={'GET', 'POST'})    
 def index():
-    return render_template('/index.html') 
+    if request.method == 'POST':
+        nombre = request.form['Nombre'] 
+        return render_template('/index.html', nombre = nombre)
+    else: 
+        return render_template('/index.html') 
 
 @app.route('/agenda', methods=['GET'])    
 def agenda():
