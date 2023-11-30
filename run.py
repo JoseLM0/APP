f6972a25c2082b1b248f8871d65c02c5a4740c23
JoseLM0 <152379622+JoseLM0@users.noreply.github.com>
2023-11-30 10:55:10 +0100

    SOLICITUDES GET Y POST

--- run.py
+++ run.py
@@ -1,14 +1,24 @@
-from flask import Flask, render_template           
+from flask import Flask, render_template, request              
 
 app = Flask(__name__)    
+#INICIO
+@app.route('/', methods={'GET', 'POST'})    
+def index():
+    if request.method == 'POST':
+        nombre = request.form['Nombre'] 
+        return render_template('/index.html', nombre = nombre)  
+    else:  
+        return render_template('/index.html') 
 
-@app.route('/', methods={'GET'})    
-def holamundo():
-    return render_template('/index.html') 
+#AGENDA
+@app.route('/agenda', methods=['GET'])    
+def agenda():
+    return render_template('/agenda.html')
 
-@app.route('/mis_proyectos', methods=['GET'])    
-def mostrarproyectos():
-    return 'Aqui se muestran los proyectos'
+#Contato
+@app.route('/Contacto')
+def contacto():
+    return render_template('/contacto.html')    
 
 if __name__ == '__main__':
     app.run(debug=True, host = '127.0.0.1', port = '5001')  
