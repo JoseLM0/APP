df5eab5124d03f10ff4fc19a69a325fdcab8cff8
JoseLM0 <152379622+JoseLM0@users.noreply.github.com>
2023-12-01 09:37:50 +0100

    0.07

--- run.py
+++ run.py
@@ -10,14 +10,29 @@ def index():
     else:  
         return render_template('/index.html') 
 
-
+#Agenda
 @app.route('/agenda', methods=['GET'])    
 def agenda():
-    return render_template('agenda.html')
-
+    return render_template('/agenda.html')
+#Contacto
 @app.route('/contacto', methods=['GET'])    
 def contacto():
-    return render_template('contacto.html')
+    return render_template('/contacto.html')
+
+#EQUIPOS INFORMATICOS
+@app.route('/equipos_informaticos', methods=['GET'])
+def equipos_informaticos():
+    return render_template('/e-informaticos.html')
+
+#Maquinaria
+@app.route('/maquinaria', methods=['GET'])
+def maquinaria():
+    return render_template('/maquinaria.html')
+
+#Vehiculos
+@app.route('/vehiculos', methods=['GET'])
+def vehiculos():
+    return render_template('/vehiculos.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host = '127.0.0.1', port = '5001')  
