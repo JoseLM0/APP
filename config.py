d6b059c75b73b9d0b995471b8878b7eb23431fdf
JoseLM0 <152379622+JoseLM0@users.noreply.github.com>
2023-12-14 13:11:13 +0100

    0.18

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #conexion a la base de datos
-SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:#2023_Root_#@localhost/db_contactos'
+SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:#2023_Root_#@localhost/db_ayun'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 #CLave secreta
@@ -9,6 +9,6 @@ SECRET_KEY = "#2023_CLAVE//SECRETA_#"
 MYSQL_HOST = 'localhost'
 MYSQL_USER = 'ADMIN'
 MYSQL_PASSWORD = '#2023_Root_#'
-MYSQL_DB = 'db_contactos'
+MYSQL_DB = 'db_ayun'
 HEX_SEC_KEY= '#2023_CLAVE//SECRETA_#'
 
